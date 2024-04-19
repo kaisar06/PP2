63756cf8792adbf8c3580e327bd55ac5a573a187
kaisar06 <150443131+kaisar06@users.noreply.github.com>
2024-04-19 19:12:57 +0500

    Add files via upload

--- lab8/Snake.py
+++ lab8/Snake.py
@@ -1,122 +1,142 @@
 import pygame
 import random
 
+#initialization
+pygame.init()
 
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 400
-CELL_SIZE = 20
-GRID_WIDTH = SCREEN_WIDTH // CELL_SIZE
-GRID_HEIGHT = SCREEN_HEIGHT // CELL_SIZE
-FPS = 5  
-SCORE_PER_FOOD = 10
-FOOD_COLOR = (255, 0, 0)
-SNAKE_COLOR = (0, 255, 0)
-BACKGROUND_COLOR = (0, 0, 0)
-SPEED_INCREMENT = 0.1  
+#colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 
-UP = (0, -1)
-DOWN = (0, 1)
-LEFT = (-1, 0)
-RIGHT = (1, 0)
+#dimensions
+WIDTH = 800
+HEIGHT = 600
 
-class SnakeGame:
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption("Snake Game")
-        self.clock = pygame.time.Clock()
-        self.snake = [(GRID_WIDTH // 2, GRID_HEIGHT // 2)]
-        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-        self.food = self.generate_food()
-        self.score = 0
-        self.level = 1
-        self.speed = FPS
+#block size
+block_size = 25
 
-    def generate_food(self):
-        while True:
-            food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-            if food not in self.snake:
-                return food
+#speed of snake
+snake_speed = 7
 
-    def draw_grid(self):
-        for x in range(0, SCREEN_WIDTH, CELL_SIZE):
-            pygame.draw.line(self.screen, (40, 40, 40), (x, 0), (x, SCREEN_HEIGHT))
-        for y in range(0, SCREEN_HEIGHT, CELL_SIZE):
-            pygame.draw.line(self.screen, (40, 40, 40), (0, y), (SCREEN_WIDTH, y))
+#display
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Snake")
 
-    def draw_snake(self):
-        for segment in self.snake:
-            pygame.draw.rect(self.screen, SNAKE_COLOR, (segment[0] * CELL_SIZE, segment[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+#game speed
+Clock = pygame.time.Clock()
 
-    def draw_food(self):
-        pygame.draw.rect(self.screen, FOOD_COLOR, (self.food[0] * CELL_SIZE, self.food[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+#font of text
+font = pygame.font.SysFont(None,25)
 
-    def check_collision(self):
-        if self.snake[0] in self.snake[1:] or not (0 <= self.snake[0][0] < GRID_WIDTH and 0 <= self.snake[0][1] < GRID_HEIGHT):
-            return True
-        return False
+def drawgrid():
+    for x in range(0, WIDTH, block_size):
+        pygame.draw.line(screen, BLACK, (x,0), (x,HEIGHT))
+    for y in range(0, HEIGHT, block_size):
+        pygame.draw.line(screen, BLACK, (0,y), (WIDTH,y))
 
-    def level_up(self):
-        if self.score % 30 == 0:
-            self.level += 1
-            self.speed += SPEED_INCREMENT 
-            self.clock.tick(int(self.speed))  
 
-    def reset(self):
-        self.snake = [(GRID_WIDTH // 2, GRID_HEIGHT // 2)]
-        self.direction = random.choice([UP, DOWN, LEFT, RIGHT])
-        self.food = self.generate_food()
-        self.score = 0
-        self.level = 1
-        self.speed = FPS
+def mainloop():
+    gameover = False
+    gameclose = False
 
-    def run(self):
-        running = True
-        while running:
+    #main coordinates of snake^s head
+    lead_x = 400
+    lead_y = 300
+
+    # Set initial movement direction of the snake
+    lead_x_change = 0
+    lead_y_change = 0
+
+    # Set initial length of the snake
+    snake_list = []
+    snake_length = 1
+
+    #random coordinates of food
+    rand_food_x = round(random.randrange(0,775)/25)*25
+    rand_food_y = round(random.randrange(0,575)/25)*25
+
+    #initialize score
+    SCORE = 0
+
+    while not gameover:
+
+        while gameclose == True:
+            screen.fill(WHITE)
+            screen_text = font.render("Game over, press C to play again or Q to quit", True, RED)
+            screen.blit(screen_text,(100, 100))
+            pygame.display.update()
+
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_UP and self.direction != DOWN:
-                        self.direction = UP
-                    elif event.key == pygame.K_DOWN and self.direction != UP:
-                        self.direction = DOWN
-                    elif event.key == pygame.K_LEFT and self.direction != RIGHT:
-                        self.direction = LEFT
-                    elif event.key == pygame.K_RIGHT and self.direction != LEFT:
-                        self.direction = RIGHT
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        gameover = True
+                        gameclose = False
+                    if event.key == pygame.K_c:
+                        mainloop()
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                gameover = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    lead_x_change = -block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    lead_x_change = block_size
+                    lead_y_change = 0
+                elif event.key == pygame.K_UP:
+                    lead_x_change = 0
+                    lead_y_change = -block_size
+                elif event.key == pygame.K_DOWN:
+                    lead_x_change = 0
+                    lead_y_change = block_size
+        
+        if lead_x>=WIDTH or lead_x < 0 or lead_y>=HEIGHT or lead_y < 0:
+            gameclose = True 
+        lead_x += lead_x_change
+        lead_y += lead_y_change
+        screen.fill(WHITE)
+        drawgrid()
+        pygame.draw.rect(screen,GREEN, [rand_food_x,rand_food_y,block_size,block_size])
 
-           
-            x, y = self.snake[0]
-            dx, dy = self.direction
-            new_head = (x + dx, y + dy)
-            self.snake.insert(0, new_head)
+        snake_head = []
+        snake_head.append(lead_x)
+        snake_head.append(lead_y)
+        snake_list.append(snake_head)
 
-      
-            if self.check_collision():
-                self.reset()
-                continue
+        if len(snake_list)>snake_length:
+            del snake_list[0]
+        for segment in snake_list[:-1]:
+            if segment == snake_head:
+                game_close = True
+        #draw snake
+        for segment in snake_list:
+            pygame.draw.rect(screen, BLACK, [segment[0], segment[1], block_size, block_size])
+        
+        #display score
+        score_text = font.render("Score: " + str(SCORE),True,BLACK)
+        screen.blit(score_text,(10,10))
 
-          
-            if new_head == self.food:
-                self.score += SCORE_PER_FOOD
-                self.food = self.generate_food()
-                self.level_up()  
-            else:
-                self.snake.pop()
+        pygame.display.update()
 
-            
-            self.screen.fill(BACKGROUND_COLOR)
-            self.draw_grid()
-            self.draw_snake()
-            self.draw_food()
-            pygame.display.flip()
+        if lead_x == rand_food_x and lead_y == rand_food_y:
+            rand_food_x = round(random.randrange(0,778)/25)*25
+            rand_food_y = round(random.randrange(0,575)/25)*25
+            snake_length += 1
+            SCORE += 1
+        Clock.tick(snake_speed)
 
-            
-            self.clock.tick(int(self.speed))
-
-        pygame.quit()
+    pygame.quit()
+    quit()
 
 
-if __name__ == "__main__":
-    SnakeGame().run()
+
+mainloop()
+
+
+
+
+                
